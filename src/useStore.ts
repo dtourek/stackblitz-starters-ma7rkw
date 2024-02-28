2feffcc4aa308a8f2fb489642e32a9da6cae0b2d
David <david.goliard@beafuture.com>
2024-02-28 12:04:09 +0100

    tradeChicken, tradeEggs, tradeRooster actions added

--- src/useStore.ts
+++ src/useStore.ts
@@ -24,14 +24,28 @@ type IGameState =
     | 'trade'
     | 'evaluateEndOfTurn';
 
+const canTradeEggs = (player: IPlayer) => player.egg >= 3;
+const canTradeChicken = (player: IPlayer) => player.chicken >= 3;
+const tradeEggs = (player: IPlayer) => {
+    const result = {...player};
+    result.egg -=3;
+    result.chicken +=1;
+} 
+const tradeChicken = (player: IPlayer) => {
+    const result = {...player};
+    result.chicken -=3;
+    result.hens +=1;
+} 
 
 type IRoll = {action: 'roll'}
-type ITrade = {action: 'trade', payload: { playerId: number, trade: 'egg' | 'chicken' | 'hens' | 'rooster' }}
 type ISwitchTurns = {action: 'switchTurns'}
 type ITradeOrRoll = {action: 'tradeOrRoll'}
+type ITradeChicken = {action: 'tradeChicken'}
+type ITradeEggs = {action: 'tradeEggs'}
 type IEvaluate = {action: 'evaluateEndOfTurn'}
+type IRoosterAttack = {action: 'roosterAttack'}
 
-type IReducerActions = IRoll | ITrade | ISwitchTurns | ITradeOrRoll | IEvaluate;
+type IReducerActions = IRoll |  ISwitchTurns | ITradeOrRoll | IEvaluate | ITradeChicken | ITradeEggs | IRoosterAttack;
 
 const storeReducer = (state: IStore, payload: IReducerActions) => {
     const dice = diceService()
@@ -48,10 +62,29 @@ const storeReducer = (state: IStore, payload: IReducerActions) => {
             return { ...state, gameState: 'evaluateEndOfTurn', players: state.players.map((player) => state.activePlayerId === player.id ? {
                       ...player,
                       rolls: [...player.rolls, dice.roll()] } : player) };
-        case 'trade':
-            // TODO add trade system
-            return { ...state, gameState: 'evaluateEndOfTurn' };
-
+        case 'tradeEggs':
+            return { ...state, gameState: 'evaluateEndOfTurn', players: state.players.map((player) => state.activePlayerId === player.id ? {
+                    ...player,
+                    egg:  player.egg -= 3,
+                    chicken:  player.chicken += 1,
+                    } : player) };
+        case 'tradeChicken':
+            return { ...state, gameState: 'evaluateEndOfTurn', players: state.players.map((player) => state.activePlayerId === player.id ? {
+                      ...player,
+                      chicken:  player.chicken -= 3,
+                      hens:  player.hens += 1
+                    } : player) };
+        case 'roosterAttack':
+            return { ...state, gameState: 'evaluateEndOfTurn', players: state.players.map((player) => state.activePlayerId === player.id ? 
+                    player.rooster ? {
+                    ...player,
+                    rooster:  false,
+                    } : 
+                    {
+                        ...player,
+                        hens: 0,
+                    }
+                    : player) };
         case 'evaluateEndOfTurn':
             return { ...state, gameState: 'switchTurns' };
 
