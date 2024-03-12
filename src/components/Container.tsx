2440a2c68345b9a4f589c1315ee5518a27807d41
davidtourek <dtourek@centrum.cz>
2024-03-12 20:53:53 +0100

    bitmap fonts added

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,8 +1,24 @@
 import {ButtonHTMLAttributes, DetailedHTMLProps, forwardRef, ReactNode} from 'react'
+import styled from "styled-components";
 
 interface IContainerProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLDivElement>, HTMLDivElement> {
     children: ReactNode
 }
-export const Container = forwardRef(({children, ...props}: IContainerProps) => {
-    return (<div {...props} style={{ display: 'flex', justifyContent: 'center' }} >{children}</div>)
+
+const ContainerDiv = styled.div`
+  display: flex;
+  justify-content: center;
+
+  @font-face {
+    font-family: 'Mono';
+    src: url('fonts/Mono.ttf') format('truetype');
+  }
+
+  h1, h2, h3, h4, h5, h6, p, a, span, div, button, input, label, select, textarea {
+    font-family: Mono;
+  }
+`
+
+export const Container = forwardRef(({children, ...props}: IContainerProps, ref) => {
+    return (<ContainerDiv  ref={ref} {...props} style={{  }} >{children}</ContainerDiv>)
 })
